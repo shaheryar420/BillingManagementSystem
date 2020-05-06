320f0a3189bd19fd522c97b2eee38162b555ecd9
usman880 <usmansafdar880@gmail.com>
2020-05-06 16:27:44 +0500

    dashboard binded

--- BillingManagementSystem/Controllers/HomeController.cs
+++ BillingManagementSystem/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using BillingManagementSystem.App_Start;
+using BillingManagementSystem.DataHelpers;
+using BillingManagementSystem.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace BillingManagementSystem.Controllers
@@ -14,6 +17,13 @@ namespace BillingManagementSystem.Controllers
         {
             return View();
         }
-       
+        public ActionResult DashBoardStats()
+        {
+            DashBoardHelpers helper = new DashBoardHelpers();
+            var response = helper.DashBoardStats();
+            var json = Json(response);
+            json.MaxJsonLength = int.MaxValue;
+            return json;
+        }
     }
 }
