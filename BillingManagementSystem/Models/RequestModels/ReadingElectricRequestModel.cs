6f40a7355f80f3f8029b7f5e9e6a585850d137bb
Musa Bin Shabeer <musabinshabeer@gmail.com>
2020-05-10 16:44:46 +0500

    Added Feature to add reading Log

--- BillingManagementSystem/Models/RequestModels/ReadingElectricRequestModel.cs
+++ BillingManagementSystem/Models/RequestModels/ReadingElectricRequestModel.cs
@@ -7,6 +7,7 @@ namespace BillingManagementSystem.Models
 {
     public class ReadingElectricRequestModel
     {
+        public string userId { get; set; }
         public string amount { get; set; }
         public string readingpicture_data { get; set; }
         public string readingpicture_type { get; set; }
