fda4124dbbe523e6b706dde5f4a5ae962f257adc
Musa Bin Shabeer <musabinshabeer@gmail.com>
2020-05-13 01:39:34 +0500

    Addget Method For Approved payments

---
+++ BillingManagementSystem/Models/ResponseModels/ApprovePaymentResponseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BillingManagementSystem.Models
+{
+    public class ApprovePaymentResponseModel
+    {
+        public string paymentId { get; set; }
+        public string residentRank { get; set; }
+        public string residentName { get; set; }
+        public string residentPaNo { get; set; }
+        public string meterNo { get; set; }
+        public string paymentDatetime { get; set; }
+        public string paymentAmount { get; set; }
+        public string fk_paymentType { get; set; }
+        public string fk_resident { get; set; }
+        public string paymentMonth { get; set; }
+        public string remarks { get; set; }
+        public string resultCode { get; set; }
+    }
+}
